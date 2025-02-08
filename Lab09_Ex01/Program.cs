84bfba65a0247b7737c947526d53293172037543
Salinatooninter <66030284@kmitl.ac.th>
2025-02-08 20:44:33 +0700

    update

--- Lab09_Ex01/Program.cs
+++ Lab09_Ex01/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Person.name = "sronsawan";  
-Console.WriteLine(Person.GetName());  
+System.Console.WriteLine(Person.GetName());  
 
 static class Person
 {
