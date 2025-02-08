85ef3aeccf1d022daebb2746eec2b3ff43b44b3d
Salinatooninter <66030284@kmitl.ac.th>
2025-02-08 20:43:08 +0700

    update

--- Lab09_Ex04/Program.cs
+++ Lab09_Ex04/Program.cs
@@ -7,10 +7,10 @@ var a2 = new AAA();
     a2.b1 = 30;
     a2.b2 = 40;  
 
-    Console.WriteLine($"a1.b1 = {a1.b1}");  
-    Console.WriteLine($"a1.b2 = {a1.b2}");  
-    Console.WriteLine($"a2.b1 = {a2.b1}");  
-    Console.WriteLine($"a2.b2 = {a2.b2}");  
+    System.Console.WriteLine($"a1.b1 = {a1.b1}");  
+    System.Console.WriteLine($"a1.b2 = {a1.b2}");  
+    System.Console.WriteLine($"a2.b1 = {a2.b1}");  
+    System.Console.WriteLine($"a2.b2 = {a2.b2}");  
     
 class AAA
 {
